22c9bc272aea51288d4ef724af6e9c13a67e2af0
Zyro <emsocontact@gmail.com>
2020-12-11 10:29:45 +0100

    Project Made

---
+++ PoopEngine/PoopEngine/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PoopEngine
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
